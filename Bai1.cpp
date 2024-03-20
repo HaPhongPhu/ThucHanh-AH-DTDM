faeb5af5558a2a9cae29f50394b414ec26475c2b
HaPhongPhu <145882827+HaPhongPhu@users.noreply.github.com>
2024-03-20 15:58:51 +0700

    Add files via upload

---
+++ Bai1.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+void printMultiplesOfSeven() {
+	int num;
+	printf("Các số nguyên có 2 chữ số và là bội của 7:\n");
+	for (num = 14; num < 100; num += 7) {
+		printf("%d ", num);
+	}
+	printf("\n");
+}
+
+int main() {
+	printMultiplesOfSeven();
+	return 0;
+}
